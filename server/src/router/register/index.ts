be5bfe780e70f66adf312392d706cc213c9651d1
ewerton11 <ewertonreisc0@gmail.com>
2022-12-28 13:42:13 -0300

    verifications

--- server/src/router/register/index.ts
+++ server/src/router/register/index.ts
@@ -1,31 +1,59 @@
 import { Request, Response } from "express"
 import { connectionDB } from "../../../database/db"
 
+export interface Body {
+  name: string
+  email: string
+  password: string
+}
+
+interface Results {
+  map(arg0: (item: { email: [] }) => []): []
+}
+
 export function register(req: Request, res: Response) {
-  const { name, email, password } = req.body
+  const { name, email, password } = req.body as Body
 
-  connectionDB.query("SELECT * FROM users", (error, results, fields) => {
-    if (error) {
-      throw error
-    }
+  connectionDB.query(
+    "SELECT * FROM users",
+    (error: Error, results: Results, fields) => {
+      if (error) {
+        throw error
+      }
 
-    const list = results.map((item) => item.email)
+      if (!name) {
+        res.status(400).send({ message: "invalid name" })
+        return
+      }
 
-    const emailExisting = list.some((item) => item === email)
+      if (!email) {
+        res.status(400).send({ message: "invalid email" })
+        return
+      }
 
-    if (emailExisting) {
-      res.status(400).send({ message: "Email already exists" })
-    } else {
-      connectionDB.query(
-        "INSERT INTO users SET ?",
-        { name, email, password },
-        (insertError, insertResults) => {
-          if (insertError) {
-            throw insertError
+      if (!password) {
+        res.status(400).send({ message: "invalid password" })
+        return
+      }
+
+      const list = results.map((item) => item.email)
+
+      const emailExisting = list.some((item) => item === email)
+
+      if (emailExisting) {
+        res.status(400).send({ message: "Email already exists" })
+      } else {
+        connectionDB.query(
+          "INSERT INTO users SET ?",
+          { name, email, password },
+          (insertError, insertResults) => {
+            if (insertError) {
+              throw insertError
+            }
+            res.send({ message: "Email added successfully" })
           }
-          res.send({ message: "Email added successfully" })
-        }
-      )
+        )
+      }
     }
-  })
+  )
 }
